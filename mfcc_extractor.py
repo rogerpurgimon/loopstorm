49f1c9abbd588f312f4d26969dcb30d492abe1bc
Edgar Espinos <edgar.espinos01@estudiant.upf.edu>
2022-05-17 12:16:56 +0200

    update

--- mfcc_extractor.py
+++ mfcc_extractor.py
@@ -1,11 +1,27 @@
+import essentia
+import essentia.standard as es
+import essentia.streaming as ess
+#import matplotlib.pyplot as plt
+from pathlib import Path
+import time
+
+
 def extract_mfcc(audio):
-    w = Windowing(type = 'blackmanharris62')
-    spectrum = Spectrum()
-    mfcc = essentia.standard.MFCC()
-    mfccs =[]
-    audio = essentia.array(audio)
-    for frame in FrameGenerator(audio, frameSize = 2048 , hopSize = 1024):
-        mfcc_bands, mfcc_coeffs = mfcc(spectrum(w(frame)))
-        mfccs.append(mfcc_coeffs)
-    mfccs = essentia.array(mfccs).T
-    return mfccs
+	
+	script_dir = Path().resolve()
+	audio_file = audio
+	
+	start_time = time.time()
+	
+	audio = es.MonoLoader(filename=str(audio_file))()
+	w = es.Windowing(type = 'blackmanharris62', zeroPadding=2048)
+	spectrum = es.Spectrum()
+	mfcc = es.MFCC()
+	mfccs =[]
+		
+	for frame in FrameGenerator(audio, frameSize = 2048 , hopSize = 1024):
+		mfcc_bands, mfcc_coeffs = mfcc(spectrum(w(frame)))
+		mfccs.append(mfcc_coeffs)
+	mfccs = essentia.array(mfccs).T
+	
+	return mfccs
