984cfbc294f2953fb558511731229b8fb8429b50
rogerpurgimon <roger.purgimon01@estudiant.upf.edu>
2022-05-30 14:20:15 +0200

    Starting audio manager

--- src/AudioManager.py
+++ src/AudioManager.py
@@ -3,7 +3,7 @@ from AudioExtractor import AudioExtractor
 
 class AudioManager:
 
-    def __init(self):
+    def __init__(self):
         self.d = {}
         self.d['main_loop'] = {}
         self.d['main_loop']['slices'] = {}
@@ -12,3 +12,18 @@ class AudioManager:
 
     def __call__(self):
         print(self.d)
+
+    def add_slice(self,loop,type_loop):
+        extractor_loop = AudioExtractor(loop)
+        onsets_hfc = extractor_loop.get_onsets()
+        extractor_loop.get_slices(onsets_hfc)
+
+        for nslice in range(len(extractor_loop.chopList)):
+            self.d[type_loop]['slices'][nslice] = extractor_loop.chopList[nslice]
+
+    def add_mfccs(self,slice,type_loop):
+
+        return False
+
+    def similarity(self):
+        return False
--- src/test.py
+++ src/test.py
@@ -4,19 +4,13 @@ import IPython.display
 import matplotlib.pyplot as plt
 
 from AudioExtractor import AudioExtractor
-from mfcc_extractor import *
+from AudioManager import AudioManager
 
 if __name__ == '__main__':
     loop = MonoLoader(filename='../loops/audio2.wav')()
-    loop1 = AudioExtractor(loop)
-    onsets_hfc = loop1.get_onsets()
-    loop1.get_slices(onsets_hfc)
+    manager = AudioManager()
+    manager.add_slice(loop,'main_loop')
+    print(manager.d['main_loop']['slices'][2])
 
-    # chopList contain the list of slices, separately
-    slice = loop1.chopList[0] #first slice
-
-
-    loop1.extract_mfcc(slice)
-    print("The mfccs of the first slice are:", loop1.mfccs)
 
 
