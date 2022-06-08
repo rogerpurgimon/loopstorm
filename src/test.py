a2badc1a4d776876f9634a5b0c284958836040e2
rogerpurgimon <roger.purgimon01@estudiant.upf.edu>
2022-06-08 16:02:58 +0200

    replace slices by similarity

--- src/test.py
+++ src/test.py
@@ -28,11 +28,42 @@ if __name__ == '__main__':
     #compute euclidean distance and save the index and specific loop of the most similar slice among all the slices
     manager.similarity()
 
-    print(manager.d['main_loop']['slices'][1]['info']) #slice audio information of the first slice in main loop (not time stamps)
+    #print(manager.d['main_loop']['slices'][1]['info']) #slice audio information of the first slice in main loop (not time stamps)
                                                         #(conté el data perquè tu li passis això i et faci un display de la slice)
 
-    print('\n', manager.d['main_loop']['slices'][1]['mfccs'])
-    print('\n', manager.d['main_loop']['slices'][1]['similarity'])
+    #print('\n', manager.d['main_loop']['slices'][1]['mfccs'])
+    #print('\n', manager.d['main_loop']['slices'][1]['similarity'])
+
+    print(manager.d['main_loop']['slices'][0]['info'])
+
+
+    ########################################################################
+
+    #REPLACE SLICES BY SIMILARITY
+
+    for i in range(len(manager.d['main_loop']['slices'])):
+        if manager.d['main_loop']['slices'][i]['similarity'][0] == 'slave_loop1':
+            loop_label = 'slave_loop1'
+            idx = manager.d['main_loop']['slices'][i]['similarity'][1]
+
+            manager.d['main_loop']['slices'][i]['info'] = manager.d[loop_label]['slices'][idx]['info']
+
+
+        elif manager.d['main_loop']['slices'][i]['similarity'][0] == 'slave_loop2':
+            loop_label = 'slave_loop2'
+            idx = manager.d['main_loop']['slices'][i]['similarity'][1]
+
+            manager.d['main_loop']['slices'][i]['info'] = manager.d[loop_label]['slices'][idx]['info']
+
+        elif manager.d['main_loop']['slices'][i]['similarity'][0] == 'slave_loop3':
+            loop_label = 'slave_loop3'
+            idx = manager.d['main_loop']['slices'][i]['similarity'][1]
+
+            manager.d['main_loop']['slices'][i]['info'] = manager.d[loop_label]['slices'][idx]['info']
+
+    print(manager.d['main_loop']['slices'][0]['info'])
+
+    #######################################################################################
 
 
 
